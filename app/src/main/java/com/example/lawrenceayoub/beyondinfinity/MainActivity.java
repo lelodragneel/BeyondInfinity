a46e17c626b35f1489fedbbc099a397ffcaf1ec0
lelodragneel <lelo.saiyan@gmail.com>
2018-01-21 16:55:21 -0400

    add map activity
    minor gui adjustments

--- app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MainActivity.java
+++ app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MainActivity.java
@@ -8,19 +8,17 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.lawrenceayoub.beyondinfinity.MESSAGE";
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText_test = (EditText) findViewById(R.id.editText_test);
-        String message = editText_test.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
+    public void openMap(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+//        EditText editText_test = (EditText) findViewById(R.id.editText_test);
+//        String message = editText_test.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MapActivity.java
+++ app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MapActivity.java
@@ -5,19 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message_acivity);
+        setContentView(R.layout.activity_map);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
-        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+//        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText(message);
+//        TextView textView = (TextView) findViewById(R.id.textView);
+//        textView.setText(message);
     }
 }
