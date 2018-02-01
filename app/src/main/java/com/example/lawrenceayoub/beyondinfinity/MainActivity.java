2878df5b9c5a43dea18d029c034777a636690b94
lelodragneel <lelo.saiyan@gmail.com>
2018-01-31 22:24:12 -0400

    major feature additions
    revamp ui
    fix app from crashing due to theme
    add map intent with back button
    remove status bar
    open in user landscape only
    utilize external library for radial progress

--- app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MainActivity.java
+++ app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MainActivity.java
@@ -3,8 +3,14 @@ package com.example.lawrenceayoub.beyondinfinity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import static android.view.View.SYSTEM_UI_FLAG_IMMERSIVE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // remove status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Intent intent = getIntent();
     }
 
     public void openMap(View view) {
+        //TextView textView_nextLevel = findViewById(R.id.textView_level);
+        //textView_nextLevel.setText("5");
         Intent intent = new Intent(this, MapActivity.class);
 //        EditText editText_test = (EditText) findViewById(R.id.editText_test);
 //        String message = editText_test.getText().toString();
--- app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MapActivity.java
+++ app/src/main/java/com/example/lawrenceayoub/beyondinfinity/MapActivity.java
@@ -3,6 +3,8 @@ package com.example.lawrenceayoub.beyondinfinity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MapActivity extends AppCompatActivity {
@@ -11,6 +13,9 @@ public class MapActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+        // remove status bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
@@ -19,5 +24,13 @@ public class MapActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
 //        TextView textView = (TextView) findViewById(R.id.textView);
 //        textView.setText(message);
+    }
+
+    public void returnToMain(View view) {
+        //Intent intent = new Intent(this, MainActivity.class);
+//        EditText editText_test = (EditText) findViewById(R.id.editText_test);
+//        String message = editText_test.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        finish();
     }
 }
